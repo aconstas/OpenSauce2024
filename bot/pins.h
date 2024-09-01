d57490b0099a9125886d4fc5c77fa12214ce5d8b
pwoosam <pwoosam5@icloud.com>
2024-08-31 21:20:02 -0700

    Update pins.h

--- bot/pins.h
+++ bot/pins.h
@@ -2,16 +2,16 @@
 #define PINS_H
 
 // Pin definitions
-#define ESC_LEFT_PIN (9)                                  // Pin for the ESC left motor
-#define ESC_RIGHT_PIN (10)                                // Pin for the ESC right motor
+#define ESC_LEFT_PIN (9)    // Pin for the ESC left motor
+#define ESC_RIGHT_PIN (10)  // Pin for the ESC right motor
 
 
-#define PHOTO_PIN_1 (14) // First photo sensor
-#define PHOTO_PIN_2 (15) // Second photo sensor
-#define PHOTO_PIN_3 (16) // Third photo sensor
+#define PHOTO_PIN_1 (14)  // A0 - First photo sensor
+#define PHOTO_PIN_2 (15)  // A1 - Second photo sensor
+#define PHOTO_PIN_3 (16)  // A2 - Third photo sensor
 
 #define LED_R_PIN (3)
 #define LED_G_PIN (5)
 #define LED_B_PIN (6)
 
-#endif // PINS_H
+#endif  // PINS_H
