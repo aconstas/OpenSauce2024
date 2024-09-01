f12295dadaa19711c6c41c93f9bfe0f5cdfde337
aconstas <constasboy@gmail.com>
2024-08-31 18:40:52 -0700

    add PhotoSensor setup/read functions

--- bot/bot.ino
+++ bot/bot.ino
@@ -1,10 +1,13 @@
-#include "esc_controller.h";
+#include "esc_controller.h"
+#include "photo_meter.h"
 
 void setup() {
-  // Serial.begin(115200);
+  Serial.begin(115200);
   setupESC();
+  setupPhotoSensor();
 }
 
 void loop() {
-  operateESC();
+  readPhotoValues();
+  // operateESC();
 }
