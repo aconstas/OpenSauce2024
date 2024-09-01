56a152769c643e09c8e8a5841504ad422b14378c
aconstas <constasboy@gmail.com>
2024-08-31 17:34:25 -0700

    read photometer value

--- bot/photo_meter.cpp
+++ bot/photo_meter.cpp
@@ -1,1 +1,33 @@
-// reads some numbers
+const int photoPin1 = A0; // First photo sensor
+const int photoPin2 = A1; // Second photo sensor
+const int photoPin3 = A2; // Third photo sensor
+
+void setup()
+{
+  Serial.begin(115200); // Initialize serial communication
+
+  for (int i = 2; i < 11; i++)
+  {
+    pinMode(i, OUTPUT);
+    digitalWrite(i, HIGH);
+  }
+
+}
+
+void loop()
+{
+  // Read values from all three sensors
+  int leftSensorValue = analogRead(photoPin1);
+  int rightSensorValue = analogRead(photoPin2);
+  int frontSensorValue = analogRead(photoPin3);
+
+  // Print the values to serial monitor
+  Serial.print("leftSensorValue: ");
+  Serial.print(leftSensorValue);
+  Serial.print("  rightSensorValue: ");
+  Serial.print(rightSensorValue);
+  Serial.print("  frontSensorValue: ");
+  Serial.println(frontSensorValue);
+
+  delay(1000); // Wait for a second before next reading
+}
