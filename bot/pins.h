74a9d68eec605996d16f786364a1242dd64c02b3
aconstas <constasboy@gmail.com>
2024-08-31 18:42:28 -0700

    define photo sensors

--- bot/pins.h
+++ bot/pins.h
@@ -5,4 +5,9 @@
 #define ESC_LEFT_PIN (9)                                  // Pin for the ESC left motor
 #define ESC_RIGHT_PIN (10)                                // Pin for the ESC right motor
 
+
+#define PHOTO_PIN_1 (14) // First photo sensor
+#define PHOTO_PIN_2 (15) // Second photo sensor
+#define PHOTO_PIN_3 (16) // Third photo sensor
+
 #endif // PINS_H
